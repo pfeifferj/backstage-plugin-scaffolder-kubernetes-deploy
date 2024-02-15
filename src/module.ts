ed3a8549b4e2d929903269dedba439253aa11901
Josephine Pfeiffer <hi@josie.lol>
2024-02-15 13:49:10 +0100

    add new backend module

---
+++ src/module.ts
@@ -0,0 +1,33 @@
+import {
+	createBackendModule,
+	coreServices,
+} from '@backstage/backend-plugin-api';
+import { ScmIntegrations } from '@backstage/integration';
+import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+import { deployKubernetesAction } from './actions';
+
+/**
+ * @public
+ * The Read File Module for the Scaffolder Backend
+ */
+export const readFileModule = createBackendModule({
+	moduleId: 'read-file',
+	pluginId: 'scaffolder',
+	register({ registerInit }) {
+		registerInit({
+			deps: {
+				scaffolderActions: scaffolderActionsExtensionPoint,
+				config: coreServices.rootConfig,
+			},
+			async init({ scaffolderActions, config }) {
+				const integrations = ScmIntegrations.fromConfig(config);
+				scaffolderActions.addActions(
+					deployKubernetesAction({
+						integrations,
+						config,
+					})
+				);
+			},
+		});
+	},
+});
